18dbe1fe5059d335af2ab7a3d11c5edf059f740e
Florian Wyss <fw@studer-raimann.ch>
2019-04-05 13:22:17 +0200

    Cron job: Fetch jira tickets

--- README.md
+++ README.md
@@ -1,2 +1,37 @@
-# HelpMeCron
+## Installation
 
+First install and enable [HelpMe](https://github.com/studer-raimann/HelpMe).
+
+### Install HelpMeCron-Plugin
+Start at your ILIAS root directory
+```bash
+mkdir -p Customizing/global/plugins/Services/Cron/CronHook
+cd Customizing/global/plugins/Services/Cron/CronHook
+git clone https://github.com/studer-raimann/HelpMeCron.git HelpMeCron
+```
+Update and activate the plugin in the ILIAS Plugin Administration
+
+### Some screenshots
+Cron:
+![Cron](./doc/screenshots/cron.png)
+
+### Dependencies
+* ILIAS 5.3 or ILIAS 5.4
+* PHP >=7.0
+* [composer](https://getcomposer.org)
+* [HelpMe](https://github.com/studer-raimann/HelpMe)
+
+Please use it for further development!
+
+### Adjustment suggestions
+* Adjustment suggestions by pull requests
+* Adjustment suggestions which are not yet worked out in detail by Jira tasks under https://jira.studer-raimann.ch/projects/PLHM
+* Bug reports under https://jira.studer-raimann.ch/projects/PLHM
+* For external users you can report it at https://plugins.studer-raimann.ch/goto.php?target=uihk_srsu_PLHM
+
+### ILIAS Plugin SLA
+Wir lieben und leben die Philosophie von Open Source Software! Die meisten unserer Entwicklungen, welche wir im Kundenauftrag oder in Eigenleistung entwickeln, stellen wir öffentlich allen Interessierten kostenlos unter https://github.com/studer-raimann zur Verfügung.
+
+Setzen Sie eines unserer Plugins professionell ein? Sichern Sie sich mittels SLA die termingerechte Verfügbarkeit dieses Plugins auch für die kommenden ILIAS Versionen. Informieren Sie sich hierzu unter https://studer-raimann.ch/produkte/ilias-plugins/plugin-sla.
+
+Bitte beachten Sie, dass wir nur Institutionen, welche ein SLA abschliessen Unterstützung und Release-Pflege garantieren.
---
+++ classes/class.ilHelpMeCronPlugin.php
@@ -0,0 +1,89 @@
+<?php
+
+require_once __DIR__ . "/../../../../UIComponent/UserInterfaceHook/HelpMe/vendor/autoload.php";
+require_once __DIR__ . "/../vendor/autoload.php";
+
+use srag\Plugins\HelpMe\Job\FetchJiraTicketsJob;
+use srag\Plugins\HelpMe\Utils\HelpMeTrait;
+use srag\RemovePluginDataConfirm\HelpMe\PluginUninstallTrait;
+
+/**
+ * Class ilHelpMeCronPlugin
+ *
+ * @author studer + raimann ag - Team Custom 1 <support-custom1@studer-raimann.ch>
+ */
+class ilHelpMeCronPlugin extends ilCronHookPlugin {
+
+	use PluginUninstallTrait;
+	use HelpMeTrait;
+	const PLUGIN_ID = "srsucron";
+	const PLUGIN_NAME = "HelpMeCron";
+	const PLUGIN_CLASS_NAME = ilHelpMePlugin::class;
+	const REMOVE_PLUGIN_DATA_CONFIRM = false;
+	const REMOVE_PLUGIN_DATA_CONFIRM_CLASS_NAME = HelpMeRemoveDataConfirm::class;
+	/**
+	 * @var self|null
+	 */
+	protected static $instance = null;
+
+
+	/**
+	 * @return self
+	 */
+	public static function getInstance(): self {
+		if (self::$instance === null) {
+			self::$instance = new self();
+		}
+
+		return self::$instance;
+	}
+
+
+	/**
+	 * ilHelpMeCronPlugin constructor
+	 */
+	public function __construct() {
+		parent::__construct();
+	}
+
+
+	/**
+	 * @return string
+	 */
+	public function getPluginName(): string {
+		return self::PLUGIN_NAME;
+	}
+
+
+	/**
+	 * @return ilCronJob[]
+	 */
+	public function getCronJobInstances(): array {
+		return [ new FetchJiraTicketsJob() ];
+	}
+
+
+	/**
+	 * @param string $a_job_id
+	 *
+	 * @return ilCronJob|null
+	 */
+	public function getCronJobInstance(/*string*/
+		$a_job_id)/*: ?ilCronJob*/ {
+		switch ($a_job_id) {
+			case FetchJiraTicketsJob::CRON_JOB_ID:
+				return new FetchJiraTicketsJob();
+
+			default:
+				return null;
+		}
+	}
+
+
+	/**
+	 * @inheritdoc
+	 */
+	protected function deleteData()/*: void*/ {
+		// Nothing to delete
+	}
+}
