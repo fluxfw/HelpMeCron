3da0d7a4766f68e145ebb4f6cf8b20dba8f206c8
Florian Wyss <fw@studer-raimann.ch>
2019-04-15 13:25:38 +0200

    Fix ILIAS not automatic create job entry in jobs table, only if open the cron jobs GUI

---
+++ sql/dbupdate.php
@@ -0,0 +1,6 @@
+<#1>
+<?php
+// Fix ILIAS not automatic create job entry in jobs table, only if open the cron jobs GUI
+\ilCronManager::createDefaultEntry(\srag\DIC\HelpMe\DICStatic::plugin(\ilHelpMeCronPlugin::class)->getPluginObject()
+	->getCronJobInstance(\srag\Plugins\HelpMe\Job\FetchJiraTicketsJob::CRON_JOB_ID), \ilHelpMeCronPlugin::PLUGIN_NAME, "Plugins", "");
+?>
