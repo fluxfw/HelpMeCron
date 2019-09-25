f430bd897f30e315189e2049b628a09d68d0dad8
Florian Wyss <fw@studer-raimann.ch>
2019-09-25 08:45:13 +0200

    Format code

--- classes/class.ilHelpMeCronPlugin.php
+++ classes/class.ilHelpMeCronPlugin.php
@@ -12,78 +12,86 @@ use srag\RemovePluginDataConfirm\HelpMe\PluginUninstallTrait;
  *
  * @author studer + raimann ag - Team Custom 1 <support-custom1@studer-raimann.ch>
  */
-class ilHelpMeCronPlugin extends ilCronHookPlugin {
+class ilHelpMeCronPlugin extends ilCronHookPlugin
+{
 
-	use PluginUninstallTrait;
-	use HelpMeTrait;
-	const PLUGIN_ID = "srsucron";
-	const PLUGIN_NAME = "HelpMeCron";
-	const PLUGIN_CLASS_NAME = ilHelpMePlugin::class;
-	const REMOVE_PLUGIN_DATA_CONFIRM = false;
-	const REMOVE_PLUGIN_DATA_CONFIRM_CLASS_NAME = HelpMeRemoveDataConfirm::class;
-	/**
-	 * @var self|null
-	 */
-	protected static $instance = null;
-
-
-	/**
-	 * @return self
-	 */
-	public static function getInstance(): self {
-		if (self::$instance === null) {
-			self::$instance = new self();
-		}
-
-		return self::$instance;
-	}
+    use PluginUninstallTrait;
+    use HelpMeTrait;
+    const PLUGIN_ID = "srsucron";
+    const PLUGIN_NAME = "HelpMeCron";
+    const PLUGIN_CLASS_NAME = ilHelpMePlugin::class;
+    const REMOVE_PLUGIN_DATA_CONFIRM = false;
+    const REMOVE_PLUGIN_DATA_CONFIRM_CLASS_NAME = HelpMeRemoveDataConfirm::class;
+    /**
+     * @var self|null
+     */
+    protected static $instance = null;
 
 
-	/**
-	 * ilHelpMeCronPlugin constructor
-	 */
-	public function __construct() {
-		parent::__construct();
-	}
+    /**
+     * @return self
+     */
+    public static function getInstance() : self
+    {
+        if (self::$instance === null) {
+            self::$instance = new self();
+        }
+
+        return self::$instance;
+    }
 
 
-	/**
-	 * @return string
-	 */
-	public function getPluginName(): string {
-		return self::PLUGIN_NAME;
-	}
+    /**
+     * ilHelpMeCronPlugin constructor
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
 
 
-	/**
-	 * @return ilCronJob[]
-	 */
-	public function getCronJobInstances(): array {
-		return [ new FetchJiraTicketsJob() ];
-	}
+    /**
+     * @return string
+     */
+    public function getPluginName() : string
+    {
+        return self::PLUGIN_NAME;
+    }
 
 
-	/**
-	 * @param string $a_job_id
-	 *
-	 * @return ilCronJob|null
-	 */
-	public function getCronJobInstance(/*string*/
-		$a_job_id)/*: ?ilCronJob*/ {
-		switch ($a_job_id) {
-			case FetchJiraTicketsJob::CRON_JOB_ID:
-				return new FetchJiraTicketsJob();
-
-			default:
-				return null;
-		}
-	}
+    /**
+     * @return ilCronJob[]
+     */
+    public function getCronJobInstances() : array
+    {
+        return [new FetchJiraTicketsJob()];
+    }
 
 
-	/**
-	 * @inheritdoc
-	 */
-	protected function deleteData()/*: void*/ {
-		// Nothing to delete
-	}
+    /**
+     * @param string $a_job_id
+     *
+     * @return ilCronJob|null
+     */
+    public function getCronJobInstance(/*string*/
+        $a_job_id
+    )/*: ?ilCronJob*/
+    {
+        switch ($a_job_id) {
+            case FetchJiraTicketsJob::CRON_JOB_ID:
+                return new FetchJiraTicketsJob();
+
+            default:
+                return null;
+        }
+    }
+
+
+    /**
+     * @inheritdoc
+     */
+    protected function deleteData()/*: void*/
+    {
+        // Nothing to delete
+    }
 }
