8d73e8e9f50ff247baa261a850ba06f07c253532
Florian Wyss <fw@studer-raimann.ch>
2020-01-30 14:18:30 +0100

    Fix

--- plugin.php
+++ plugin.php
@@ -3,7 +3,7 @@
 require_once __DIR__ . "/../../../UIComponent/UserInterfaceHook/HelpMe/vendor/srag/dic/src/PHPVersionChecker.php";
 
 $id = "srsucron";
-$version = "1.1.0";
+$version = "1.2.0";
 $ilias_min_version = "5.3.0";
 $ilias_max_version = "5.4.999";
 $responsible = "studer + raimann ag - Team Custom 1";
