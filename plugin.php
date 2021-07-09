ef04db4b09cc7c68491dca35a48faa106393646a
fluxfw <fluxfw@fluxlabs.ch>
2021-07-09 12:08:11 +0200

    Remove ILIAS 5.4 support

--- plugin.php
+++ plugin.php
@@ -4,7 +4,7 @@
 require_once __DIR__ . "/vendor/autoload.php";
 
 $id = "srsucron";
-$version = "1.3.8";
+$version = "1.4.0";
 $ilias_min_version = "6.0";
 $ilias_max_version = "6.999";
 $responsible = "fluxlabs";
